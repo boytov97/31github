80c7d3917e88952ea2ce604c9071412cc5c51a32
boyutov <boytov97@gmail.com>
2023-12-19 20:56:14 +0600

    -feat: Bridge: on stage reading, understanding

--- src/bridge/index.ts
+++ src/bridge/index.ts
@@ -1,5 +1,5 @@
 export const inlzBridge = async () => {
-  // have to read mode. I can not understand yet.
+  // have to read more. I can not understand yet... Stil can not...
 
   return Promise.resolve({});
 };
