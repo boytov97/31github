74f0530168e8acd4d0d56f257ea58ffc271c6582
boyutov <boytov97@gmail.com>
2023-12-20 09:17:52 +0600

    -feat: Bridge: studing in progress

--- src/bridge/index.ts
+++ src/bridge/index.ts
@@ -1,5 +1,6 @@
 export const inlzBridge = async () => {
   // have to read more. I can not understand yet... Stil can not...
+  // Didn't have time to read the documentation
 
   return Promise.resolve({});
 };
